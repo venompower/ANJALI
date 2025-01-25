24bae8ee230adc1f7c5e37e373f7e8fe8f61c5db
VENOM <venommusic91@gmail.com>
2025-01-25 20:09:01 +0530

    Add files via upload

---
+++ ANJALIMUSIC/plugins/tools/download.py
@@ -0,0 +1,170 @@
+import time
+from urllib.parse import urlparse
+import os
+import asyncio
+import requests
+import wget
+import yt_dlp
+import config
+from config import *
+from youtubesearchpython import SearchVideos
+from youtube_search import YoutubeSearch
+from yt_dlp import YoutubeDL
+from bs4 import BeautifulSoup
+from pyrogram import Client, filters
+from pyrogram.types import *
+from NEXIOMUSIC import app
+
+# ------------------------------------------------------------------------------- #
+
+@app.on_message(filters.command("audio"))
+def download_song(_, message):
+    query = " ".join(message.command[1:])  
+    print(query)
+    m = message.reply("**❖ sᴇᴀʀᴄʜɪɴɢ... **")
+    ydl_ops = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        duration = results[0]["duration"]
+
+        # Add these lines to define views and channel_name
+        views = results[0]["views"]
+        channel_name = results[0]["channel"]
+
+    except Exception as e:
+        m.edit("**❖ ɴᴏ ʀᴇsᴜʟᴛs ᴡᴇʀᴇ ғᴏᴜɴᴅ. ᴍᴀᴋᴇ sᴜʀᴇ ʏᴏᴜ ᴛʏᴘᴇᴅ ᴛʜᴇ ᴄᴏʀʀᴇᴄᴛ sᴏɴɢ ɴᴀᴍᴇ**")
+        print(str(e))
+        return
+    m.edit("**❖ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...**")
+    try:
+        with yt_dlp.YoutubeDL(ydl_ops) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(float(dur_arr[i])) * secmul
+            secmul *= 60
+        m.edit("**❖ ᴜᴘʟᴏᴀᴅɪɴɢ...**")
+
+        message.reply_audio(
+            audio_file,
+            thumb=thumb_name,
+            title=title,
+            caption=f"{title}\n❖ ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ ➠ {message.from_user.mention}\n❍ ᴠɪᴇᴡs ➠ {views}\n❍ ᴄʜᴀɴɴᴇʟ ➠ {channel_name}",
+            duration=dur
+        )
+        m.delete()
+    except Exception as e:
+        m.edit(" - An error !!")
+        print(e)
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
+        
+
+def get_file_extension_from_url(url):
+    url_path = urlparse(url).path
+    basename = os.path.basename(url_path)
+    return basename.split(".")[-1]
+
+
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
+
+# ------------------------------------------------------------------------------- #
+
+@app.on_message(filters.command(["yt", "video"]))
+async def ytmusic(client, message: Message):
+    urlissed = get_text(message)
+    await message.delete()
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    sachin = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
+
+    pablo = await client.send_message(message.chat.id, f"❖ sᴇᴀʀᴄʜɪɴɢ, ᴩʟᴇᴀsᴇ ᴡᴀɪᴛ...")
+    if not urlissed:
+        await pablo.edit(
+            "❖ sᴏɴɢ ɴᴏᴛ ғᴏᴜɴᴅ ᴏɴ ʏᴏᴜᴛᴜʙᴇ.\n\n❍  ᴍᴀʏʙᴇ ᴛᴜɴᴇ ɢᴀʟᴛɪ ʟɪᴋʜᴀ ʜᴏ, ᴩᴀᴅʜᴀɪ - ʟɪᴋʜᴀɪ ᴛᴏʜ ᴋᴀʀᴛᴀ ɴᴀʜɪ ᴛᴜ !"
+        )
+        return
+
+    search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
+    mi = search.result()
+    mio = mi["search_result"]
+    mo = mio[0]["link"]
+    thum = mio[0]["title"]
+    fridayz = mio[0]["id"]
+    thums = mio[0]["channel"]
+    kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    url = mo
+    sedlyf = wget.download(kekme)
+    opts = {
+        "format": "best",
+        "addmetadata": True,
+        "key": "FFmpegMetadata",
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
+        "outtmpl": "%(id)s.mp4",
+        "logtostderr": False,
+        "quiet": True,
+    }
+    try:
+        with YoutubeDL(opts) as ytdl:
+            infoo = ytdl.extract_info(url, False)
+            round(infoo["duration"] / 60)
+            ytdl_data = ytdl.extract_info(url, download=True)
+
+    except Exception as e:
+        await pablo.edit(f"**❖ ғᴀɪʟᴇᴅ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ.** \n**❍ ᴇʀʀᴏʀ ➠** `{str(e)}`")
+        return
+    c_time = time.time()
+    file_stark = f"{ytdl_data['id']}.mp4"
+    capy = f"**❖ ᴛɪᴛʟᴇ ➠** [{thum}]({mo})\n**❍ ᴄʜᴀɴɴᴇʟ ➠** {thums}\n**❍ sᴇᴀʀᴄʜᴇᴅ ➠** {urlissed}\n**❍ ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ ➠** {sachin}"
+    await client.send_video(
+        message.chat.id,
+        video=open(file_stark, "rb"),
+        duration=int(ytdl_data["duration"]),
+        file_name=str(ytdl_data["title"]),
+        thumb=sedlyf,
+        caption=capy,
+        supports_streaming=True,
+        progress_args=(
+            pablo,
+            c_time,
+            f"❖ ᴩʟᴇᴀsᴇ ᴡᴀɪᴛ...\n\n❍ ᴜᴩʟᴏᴀᴅɪɴɢ `{urlissed}` ғʀᴏᴍ ʏᴏᴜᴛᴜʙᴇ sᴇʀᴠᴇʀs...",
+            file_stark,
+        ),
+    )
+    await pablo.delete()
+    for files in (sedlyf, file_stark):
+        if files and os.path.exists(files):
+            os.remove(files)
+
+
+__mod_name__ = "Vɪᴅᴇᴏ"
+__help__ = """ 
+/video to download video song
+/yt to download video song """
