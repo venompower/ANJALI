d06999c5b331bf9211d47a3571f0852406bc2f0d
VENOM <venommusic91@gmail.com>
2025-01-25 20:47:30 +0530

    Update help.py

--- ANJALIMUSIC/utils/inline/help.py
+++ ANJALIMUSIC/utils/inline/help.py
@@ -47,7 +47,26 @@ def help_pannel(_, START: Union[bool, int] = None):
               
             ],
             [
-                InlineKeyboardButton(text=_["H_B_15"], callback_data="help_callback hb15",),                
+                InlineKeyboardButton(text=_["H_B_15"], callback_data="help_callback hb15",),
+                InlineKeyboardButton(text=_["H_B_16"], callback_data="help_callback hb16",),                
+            ],
+            [
+                InlineKeyboardButton(text=_["H_B_17"], callback_data="help_callback hb17",),
+                InlineKeyboardButton(text=_["H_B_18"], callback_data="help_callback hb18",),
+                
+            ],
+            [
+                InlineKeyboardButton(text=_["H_B_19"], callback_data="help_callback hb19",),
+                InlineKeyboardButton(text=_["H_B_20"], callback_data="help_callback hb20",),
+                
+            ],
+            [
+               InlineKeyboardButton(text=_["H_B_21"], callback_data="help_callback hb21",),
+               InlineKeyboardButton(text=_["H_B_22"], callback_data="help_callback hb22",),
+            ],
+            [
+                InlineKeyboardButton(text=_["H_B_23"], callback_data="help_callback hb23",),
+                InlineKeyboardButton(text=_["H_B_24"], callback_data="help_callback hb24",),
             ],
             mark,
         ]
