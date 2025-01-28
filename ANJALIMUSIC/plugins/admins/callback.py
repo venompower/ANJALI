e4869f2b8ffbfd6c51c3372eae1e73cfbc93139b
VENOM <venommusic91@gmail.com>
2025-01-28 15:18:45 +0530

    Update callback.py

--- ANJALIMUSIC/plugins/admins/callback.py
+++ ANJALIMUSIC/plugins/admins/callback.py
@@ -53,9 +53,9 @@ async def unban_assistant(_, callback: CallbackQuery):
     
     try:
         await app.unban_chat_member(chat_id, userbot.id)
-        await callback.answer("𝗠𝘆 𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝘁 𝗜𝗱 𝗨𝗻𝗯𝗮𝗻𝗻𝗲𝗱 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆🥳\n\n➻ 𝗡𝗼𝘄 𝗬𝗼𝘂 𝗖𝗮𝗻 𝗣𝗹𝗮𝘆 𝗦𝗼𝗻𝗴𝘀🔉\n\n𝗧𝗵𝗮𝗻𝗸 𝗬𝗼𝘂💝", show_alert=True)
+        await callback.answer("𝖬𝖸 𝖠𝖲𝖲𝖨𝖲𝖳𝖠𝖬𝖳 𝖨𝖣 𝖴𝖭𝖡𝖠𝖭𝖭𝖤𝖣 𝖲𝖴𝖢𝖢𝖤𝖲𝖲𝖥𝖴𝖫𝖫𝖸\n\n➻ 𝖭𝖮𝖶 𝖸𝖮𝖴 𝖢𝖠𝖭 𝖯𝖫𝖠𝖸 𝖲𝖮𝖭𝖦𝖲\n\n𝖳𝖧𝖠𝖭𝖪 𝖸𝖮𝖴 𝖣𝖠𝖱𝖫𝖨𝖭𝖦", show_alert=True)
     except Exception as e:
-        await callback.answer(f"𝙁𝙖𝙞𝙡𝙚𝙙 𝙏𝙤 𝙐𝙣𝙗𝙖𝙣 𝙈𝙮 𝘼𝙨𝙨𝙞𝙨𝙩𝙖𝙣𝙩 𝘽𝙚𝙘𝙖𝙪𝙨𝙚 𝙄 𝘿𝙤𝙣'𝙩 𝙃𝙖𝙫𝙚 𝘽𝙖𝙣 𝙋𝙤𝙬𝙚𝙧\n\n➻ 𝙋𝙡𝙚𝙖𝙨𝙚 𝙋𝙧𝙤𝙫𝙞𝙙𝙚 𝙈𝙚 𝘽𝙖𝙣 𝙋𝙤𝙬𝙚𝙧 𝙎𝙤 𝙏𝙝𝙖𝙩 𝙄 𝙘𝙖𝙣 𝙐𝙣𝙗𝙖𝙣 𝙈𝙮 𝘼𝙨𝙨𝙞𝙨𝙩𝙖𝙣𝙩 𝙄𝙙", show_alert=True)
+        await callback.answer(f"𝖥𝖠𝖨𝖫𝖤𝖣 𝖳𝖮 𝖴𝖭𝖡𝖠𝖭 𝖬𝖸 𝖠𝖲𝖲𝖨𝖲𝖳𝖠𝖭𝖳 𝖡𝖤𝖢𝖠𝖴𝖲𝖤 𝖨 𝖣𝖮𝖭𝖳 𝖧𝖠𝖵𝖤 𝖡𝖠𝖭 𝖯𝖮𝖶𝖤𝖱\n\n➻ 𝖯𝖫𝖤𝖠𝖲𝖤 𝖯𝖱𝖮𝖵𝖨𝖣𝖤 𝖬𝖤 𝖡𝖠𝖭 𝖯𝖮𝖶𝖤𝖱 𝖲𝖮 𝖳𝖧𝖠𝖳 𝖨 𝖢𝖠𝖭 𝖴𝖭𝖡𝖠𝖭 𝖬𝖸 𝖠𝖲𝖲𝖨𝖲𝖳𝖠𝖭𝖳 𝖨𝖣", show_alert=True)
 
 
 @app.on_callback_query(filters.regex("ADMIN") & ~BANNED_USERS)
