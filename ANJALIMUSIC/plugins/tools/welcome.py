fa4c2c927a827e73cde744e5610a90d7a35194ff
VENOM <venommusic91@gmail.com>
2025-01-31 10:12:21 +0530

    Update welcome.py

--- ANJALIMUSIC/plugins/tools/welcome.py
+++ ANJALIMUSIC/plugins/tools/welcome.py
@@ -96,6 +96,8 @@ async def greet_group(_, member: ChatMemberUpdated):
             member.chat.id,
             photo=welcomeimg,
             caption=f"""
+
+            
 **❅───✦ ᴡєʟᴄσϻє ᴛσ ✦───❅**
    {member.chat.title}
 ▰▰▰▰▰▰▰▰▰▰▰▰▰
