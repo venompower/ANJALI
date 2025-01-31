ed7f9c9558a04d5c9ad881e467ce2477f10a584c
VENOM <venommusic91@gmail.com>
2025-01-31 10:08:49 +0530

    Update welcome.py

--- ANJALIMUSIC/plugins/tools/welcome.py
+++ ANJALIMUSIC/plugins/tools/welcome.py
@@ -81,7 +81,7 @@ async def greet_group(_, member: ChatMemberUpdated):
             user.photo.big_file_id, file_name=f"pp{user.id}.png"
         )
     except AttributeError:
-        pic = "ANJALIMUSIC/assets/ANJALIMUSIC/assets/wel2.png"
+        pic = "ANJALIMUSIC/assets/wel2.png"
     if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
         try:
             await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
