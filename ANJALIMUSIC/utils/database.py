034efd0ec40ef385823f9b3af9ae5bd5d1fc1d9d
VENOM <venommusic91@gmail.com>
2025-02-13 22:41:33 +0530

    ᴀᴀʀᴠɪ ᴍᴜsɪᴄ

--- ANJALIMUSIC/utils/database.py
+++ ANJALIMUSIC/utils/database.py
@@ -67,7 +67,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from SACHIN_MUSIC.core.userbot import assistants
+    from ANJALIMUSIC.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -81,7 +81,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from SACHIN_MUSIC.core.userbot import assistants
+    from ANJALIMUSIC.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -108,7 +108,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from SACHIN_MUSIC.core.userbot import assistants
+    from ANJALIMUSIC.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -121,7 +121,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from SACHIN_MUSIC.core.userbot import assistants
+    from ANJALIMUSIC.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
